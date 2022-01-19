37f6c58721fa163e2a6cf93fb9698c89bf40ae34
Arun-Kalyan <74956275+Arun-Kalyan@users.noreply.github.com>
2022-01-19 19:16:22 +0530

    Add files via upload

---
+++ largestpossibleodd.java
@@ -0,0 +1,54 @@
+import java.util.*;
+public class largestpossibleodd {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		long N = sc.nextLong();
+		int [] digits = new int [10];
+		while(N!=0)
+		{
+			digits[(int) (N % 10)]++;
+			N=N/10;
+		}
+		
+		int ud=-1;
+		
+		for(int i=1; i<=9; i+=2) // odd number traversal
+		{
+			if(digits[i] > 0)
+			{
+				ud = i;
+				digits[i]--;
+				break;
+			}
+		}
+		
+		if(ud==-1)
+		{
+			System.out.print("no");
+			return;
+		}
+		int start=1;
+		for(int i=1; i<=9; i++)
+		{
+			if(digits[i] > 0)
+			{
+				start=0;
+				break;
+			}
+		}
+		for(int i=9; i>=start; i--)
+		{
+			while(digits[i] > 0)
+			{
+				System.out.print(i);
+				digits[i]--;
+			}
+		}
+		
+		System.out.print(ud);
+
+	}
+
+}
---
+++ matrixzigzagtopleft.java
@@ -0,0 +1,65 @@
+import java.util.*;
+public class matrixzigzagtopleft {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int R = sc.nextInt();
+		int C = sc.nextInt();
+		int [][] mat = new int[R][C];
+		for(int i=0; i<R; i++)
+		{
+			for(int j=0; j<C; j++)
+			{
+				mat[i][j] = sc.nextInt();
+			}
+		}
+		
+		int direction = 1, row =0, col =0;
+		
+		for(int i=1; i<=R+C-1; i++)
+		{
+			if(direction == 1)
+			{
+				while(row >=0 && col<C)
+				{
+					System.out.print(mat[row][col]+" ");
+					row--;
+					col++;
+				}
+				direction = -1;
+				if(row<0 && col < C)
+				{
+					row=0;
+				}
+				if(col >= C)
+				{
+					col = C-1;
+					row +=2;
+				}
+			}
+			else
+			{
+				while(row<R && col>=0)
+				{
+					System.out.print(mat[row][col]+" ");
+					row++;
+					col--;
+				}
+
+			direction=1;
+			if(col<0 && row<R)
+			{
+				col =0;
+			}
+			
+			if(row>=R ) {
+				row = R-1;
+				col+=2;
+			}
+		}
+		}
+		
+	}
+
+}
